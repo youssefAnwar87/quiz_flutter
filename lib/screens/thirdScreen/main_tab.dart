bd00dded6662788e864e849f041785f997233846
youssefAnwar87 <yassoyosse@gmail.com>
2023-10-11 10:34:09 +0300

    edited third screen

--- lib/screens/thirdScreen/main_tab.dart
+++ lib/screens/thirdScreen/main_tab.dart
@@ -11,182 +11,183 @@ class MainTab extends StatefulWidget {
 }
 
 class _MainTabState extends State<MainTab> {
-  List<String> chipLabels = [
+  final List<String> chipLabels = [
     'Discovered',
     'News',
     'Most Viewed',
     'Saved',
     'Chip 5',
-    'Chip 6'
+    'Chip 6',
   ];
+  Set<int> selectedChipIndices = {};
+
   int selectedChipIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    CarouselProvider provider = Provider.of(context);
+    final provider = Provider.of<CarouselProvider>(context);
 
     return Scaffold(
-      resizeToAvoidBottomInset: false, // Prevent resizing when the keyboard appears
-      backgroundColor: Colors.white,
-      body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 10),
-
-          // keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-          child: Column(
+        // resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white,
+        body: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: SingleChildScrollView(
+            child: Column(
             children: [
-              SizedBox(height: MediaQuery.of(context).size.height * 0.06),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Tab(
-                    icon: Image.asset(AppAssets.flower),
-                  ),
-                  Text(
-                    "AliceCare",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-                  )
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(13),
-                            border: Border.all(color: Colors.grey)),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.purple), // Set the focused border color
-                            ),
-                            hintText:
-                                'Articles, Video, Audio and More,...', // Hint text
-                            prefixIcon: Icon(Icons.search), // Suffix icon
-                          ),
-                          // Other properties and validators
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-              Container(
-                height: MediaQuery.of(context).size.height *
-                    0.04, // Height of the chip row
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: chipLabels.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 14.0),
-                      child: ChoiceChip(
-                        label: Text(
-                          chipLabels[index],
-                          style: TextStyle(
-                              color: selectedChipIndex == index
-                                  ? Color(0xff6941c5)
-                                  : Colors.grey),
-                        ),
-                        selected: selectedChipIndex == index,
-                        selectedColor: Color(0xfff3eafe),
-                        onSelected: (bool selected) {
-                          selectedChipIndex = selected ? index : -1;
-                          provider.setCurrentIndex(index);
-                        },
-                      ),
-                    );
-                  },
-                ),
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-              Row(
-                children: [
-                  Text("Hot topics",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  Spacer(),
-                  Text(
-                    "See more",
-                    style: TextStyle(
-                      fontSize: 19,
-                      color: Color(0Xff5925DC),
-                    ),
-                  ),
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    color: Color(0Xff5925DC),
-                    size: 20,
-                  ),
-                ],
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-              Expanded(
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 10,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Container(
-                        width: MediaQuery.of(context)
-                            .size
-                            .width, // Adjust the width as needed
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                            image: AssetImage(
-                                index % 2 == 0 ? AppAssets.pic1 : AppAssets.pic2),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-              Row(
-                children: [
-                  SizedBox(width: MediaQuery.of(context).size.width * 0.01),
-                  Text("Get Tips",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                ],
-              ),
-              SizedBox(height: MediaQuery.of(context).size.width * 0.03),
-              Image.asset(AppAssets.pic3),
-              SizedBox(height: MediaQuery.of(context).size.width * 0.03),
-
-              Row(
-                children: [
-                  Text("Cycle Phases and Period",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  Spacer(),
-                  Text(
-                    "See more",
-                    style: TextStyle(
-                      fontSize: 19,
-                      color: Color(0Xff5925DC),
-                    ),
-                  ),
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    color: Color(0Xff5925DC),
-                    size: 20,
-                  ),
-                ],
-              ),
-              // SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-            ],
-          ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.06),
+    Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    Tab(
+    icon: Image.asset(AppAssets.flower),
+    ),
+    Text(
+    "AliceCare",
+    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+    ),
+    ],
+    ),
+    Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    Container(
+    decoration: BoxDecoration(
+    color: Colors.transparent,
+    borderRadius: BorderRadius.circular(13),
+    border: Border.all(color: Colors.grey),
+    ),
+    width: MediaQuery.of(context).size.width *0.85,
+    child: TextFormField(
+    decoration: InputDecoration(
+    focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.purple),
+    ),
+    hintText: 'Articles, Video, Audio and More,...',
+    prefixIcon: Icon(Icons.search),
+    ),
+    ),
+    ),
+    ],
+    ),
+    SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+    Container(
+    height: MediaQuery.of(context).size.height * 0.04,
+    child: ListView.builder(
+    scrollDirection: Axis.horizontal,
+    itemCount: chipLabels.length,
+    itemBuilder: (BuildContext context, int index) {
+    return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 14.0),
+    child: ChoiceChip(
+    label: Text(
+    chipLabels[index],
+    style: TextStyle(
+    color:  selectedChipIndices.contains(index) // Check if the index is in the set
+        ? Color(0xff6941c5)
+        : Colors.grey,
+    ),
+    ),
+    selected: selectedChipIndices.contains(index),
+    selectedColor: Color(0xfff3eafe),
+    onSelected: (bool selected) {
+      if (selected) {
+        selectedChipIndices.add(index); // Add index to set when selected
+      } else {
+        selectedChipIndices.remove(index); // Remove index from set when unselected
+      }    provider.setCurrentIndex(index);
+    },
+    ),
+    );
+    },
+    ),
+    ),
+    SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+    Row(
+    children: [
+    Text(
+    "Hot topics",
+    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+    ),
+    Spacer(),
+    Text(
+    "See more",
+    style: TextStyle(
+    fontSize: 19,
+    color: Color(0Xff5925DC),
+    ),
+    ),
+    Icon(
+    Icons.arrow_forward_ios,
+    color: Color(0Xff5925DC),
+    size: 20,
+    ),
+    ],
+    ),
+    SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+    Container(
+    height: 200, // Set an appropriate height
+    child: ListView.builder(
+    scrollDirection: Axis.horizontal,
+    itemCount: 10,
+    itemBuilder: (BuildContext context, int index) {
+    return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+    child: Container(
+    width: 300, // Adjust the width as needed
+    decoration: BoxDecoration(
+    border: Border.all(color: Colors.grey),
+    borderRadius: BorderRadius.circular(10),
+    image: DecorationImage(
+    image: AssetImage(
+    index % 2 == 0 ? AppAssets.pic1 : AppAssets.pic2,
+    ),
+    fit: BoxFit.cover,
+    ),
+    ),
+    ),
+    );
+    },
+    ),
+    ),
+    SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+    Row(
+    children: [
+    SizedBox(width: 10),
+    Text(
+    "Get Tips",
+    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+    ),
+    ],
+    ),
+    SizedBox(height: 20), // Adjust the height as needed
+    Image.asset(AppAssets.pic3),
+    SizedBox(height: 20), // Adjust the height as needed
+    Row(
+    children: [
+    Text(
+    "Cycle Phases and Period",
+    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+    ),
+    Spacer(),
+    Text(
+    "See more",
+    style: TextStyle(
+    fontSize: 19,
+    color: Color(0Xff5925DC),
+    ),
+    ),
+    Icon(
+    Icons.arrow_forward_ios,
+    color: Color(0Xff5925DC),
+    size: 20,
+    ),
+    ],
+    ),
+    ],
+    ),
+            ),
         ),
-
     );
   }
 }
